deb40660aaade35d7e5934c4766d1d7f58590076
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-02-21 19:57:45 -0300

    Criando operação de instanciação

--- src/main/java/com/criptx/cursomc/CursomcApplication.java
+++ src/main/java/com/criptx/cursomc/CursomcApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @ComponentScan(basePackages={"com.criptx.cursomc"})
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
  	private CategoriaRepository categoriaRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -20,5 +23,7 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria categoria1 = new Categoria(null, "Informatica");
 		Categoria categoria2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 }
