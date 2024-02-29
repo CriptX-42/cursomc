29bb4dc9d7d39733d1ccc2230297c6d9968feedb
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-02-28 21:33:51 -0300

    Produtos e associação muitos para muitos

--- src/main/java/com/criptx/cursomc/CursomcApplication.java
+++ src/main/java/com/criptx/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.criptx.cursomc;
 
 import com.criptx.cursomc.domain.Categoria;
+import com.criptx.cursomc.domain.Produto;
 import com.criptx.cursomc.repositories.CategoriaRepository;
+import com.criptx.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,10 @@ import java.util.Arrays;
 @ComponentScan(basePackages={"com.criptx.cursomc"})
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
- 	private CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,6 +29,18 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informatica");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 
+		Produto produto1 = new Produto(null, "computador", 2000.00);
+		Produto produto2 = new Produto(null, "Tablet", 1500.00);
+		Produto produto3 = new Produto(null, "teclado", 600.00);
+
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 }
