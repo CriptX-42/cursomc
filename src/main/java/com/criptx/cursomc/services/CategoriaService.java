446aedda65f53ab3250f559bbf1950cd8effb298
CriptX-42 <ricardo-svc@live.com>
2024-06-05 18:57:17 -0300

    Inserindo nova vategoria com POST

--- src/main/java/com/criptx/cursomc/services/CategoriaService.java
+++ src/main/java/com/criptx/cursomc/services/CategoriaService.java
@@ -1,12 +1,12 @@
 package com.criptx.cursomc.services;
-import java.util.Optional;
 
 import com.criptx.cursomc.domain.Categoria;
 import com.criptx.cursomc.repositories.CategoriaRepository;
 import com.criptx.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -18,8 +18,10 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-
-
     }
 
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
