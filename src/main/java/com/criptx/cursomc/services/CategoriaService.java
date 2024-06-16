24e0bcc89dcb9ebfe4d456eb139e126f1a3b3f27
CriptX-42 <ricardo-svc@live.com>
2024-06-16 19:12:53 -0300

    Validação sintática com Beans Validation

--- src/main/java/com/criptx/cursomc/services/CategoriaService.java
+++ src/main/java/com/criptx/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.criptx.cursomc.services;
 
 import com.criptx.cursomc.domain.Categoria;
+import com.criptx.cursomc.dto.CategoriaDTO;
 import com.criptx.cursomc.repositories.CategoriaRepository;
 import com.criptx.cursomc.services.exceptions.DataIntegrityException;
 import com.criptx.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return repo.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO (CategoriaDTO objDto) {
+        return new Categoria(objDto.getId(), objDto.getNome());
     }
 }
