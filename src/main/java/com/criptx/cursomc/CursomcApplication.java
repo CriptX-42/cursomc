8fe2133f5e67987e035ea5d9f617c090ce61a388
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-07-02 21:55:16 -0300

    Busca de pedidos por nome e categoria WIP

--- src/main/java/com/criptx/cursomc/CursomcApplication.java
+++ src/main/java/com/criptx/cursomc/CursomcApplication.java
@@ -66,16 +66,42 @@ public class CursomcApplication implements CommandLineRunner {
         Produto produto1 = new Produto(null, "computador", 2000.00);
         Produto produto2 = new Produto(null, "Tablet", 1500.00);
         Produto produto3 = new Produto(null, "teclado", 600.00);
+        Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
+        Produto p5 = new Produto(null, "Toalha", 50.00);
+        Produto p6 = new Produto(null, "Colcha", 200.00);
+        Produto p7 = new Produto(null, "TV true color", 1200.00);
+        Produto p8 = new Produto(null, "Roçadeira", 800.00);
+        Produto p9 = new Produto(null, "Abajour", 100.00);
+        Produto p10 = new Produto(null, "Pendente", 180.00);
+        Produto p11 = new Produto(null, "Shampoo", 90.00);
 
         categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
         categoria2.getProdutos().addAll(Arrays.asList(produto2));
+        categoria2.getProdutos().addAll(Arrays.asList(produto2, p4));
+        categoria3.getProdutos().addAll(Arrays.asList(p5, p6));
+        categoria4.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3, p7));
+        categoria5.getProdutos().addAll(Arrays.asList(p8));
+        categoria6.getProdutos().addAll(Arrays.asList(p9, p10));
+        categoria7.getProdutos().addAll(Arrays.asList(p11));
+
+        produto1.getCategorias().addAll(Arrays.asList(categoria1, categoria4));
+        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2, categoria4));
+        produto3.getCategorias().addAll(Arrays.asList(categoria1, categoria4));
+        p4.getCategorias().addAll(Arrays.asList(categoria2));
+        p5.getCategorias().addAll(Arrays.asList(categoria3));
+        p6.getCategorias().addAll(Arrays.asList(categoria3));
+        p7.getCategorias().addAll(Arrays.asList(categoria4));
+        p8.getCategorias().addAll(Arrays.asList(categoria5));
+        p9.getCategorias().addAll(Arrays.asList(categoria6));
+        p10.getCategorias().addAll(Arrays.asList(categoria6));
+        p11.getCategorias().addAll(Arrays.asList(categoria7));
 
         produto1.getCategorias().addAll(Arrays.asList(categoria1));
         produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
         produto3.getCategorias().addAll(Arrays.asList(categoria1));
 
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
-        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, p4, p5, p6, p7, p8, p9, p10, p11));
 
 
         Estado estado1 = new Estado(null, "Minas Gerais");
---
+++ src/main/java/com/criptx/cursomc/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.criptx.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.criptx.cursomc.domain.Categoria;
+import com.criptx.cursomc.domain.Produto;
+import com.criptx.cursomc.repositories.CategoriaRepository;
+import com.criptx.cursomc.repositories.ProdutoRepository;
+import com.criptx.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Produto find(Integer id) {
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+    }
+
+    public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+        return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+    }
+}
