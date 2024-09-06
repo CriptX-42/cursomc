e9b34ea553bdcd7109e3b0c02f6933ade1c0515f
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-09-06 19:01:42 -0300

    Autorizando endpoints para perfils especificos

--- src/main/java/com/criptx/cursomc/resource/ClienteResource.java
+++ src/main/java/com/criptx/cursomc/resource/ClienteResource.java
@@ -10,6 +10,7 @@ import com.criptx.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,12 +41,14 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>> findAll() {
         List<Cliente> list = service.findAll();
@@ -55,6 +58,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value ="/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
                                                        @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
