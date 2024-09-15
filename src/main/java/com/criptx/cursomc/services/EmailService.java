e1a8aa90724930f3ceea242509090677d7230b5e
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-09-14 21:05:09 -0300

    Esqueci a senha

--- src/main/java/com/criptx/cursomc/services/EmailService.java
+++ src/main/java/com/criptx/cursomc/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.criptx.cursomc.services;
 
+import com.criptx.cursomc.domain.Cliente;
 import com.criptx.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -14,4 +15,6 @@ public interface EmailService {
     void sendOrderConfirmationHtmlEmail(Pedido obj);
 
     void sendHtmlEmail(MimeMessage msg);
+
+    void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
