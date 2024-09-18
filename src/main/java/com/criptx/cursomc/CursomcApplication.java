6c3cb6f6f293f935ff53f0d500932c031bf693a8
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-09-18 20:16:50 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/criptx/cursomc/CursomcApplication.java
+++ src/main/java/com/criptx/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.criptx.cursomc;
 
+import com.criptx.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +16,10 @@ public class CursomcApplication implements CommandLineRunner {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
-    public void run(String... args) throws Exception {
+    @Autowired
+    private S3Service s3Service;
 
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
     }
 }
