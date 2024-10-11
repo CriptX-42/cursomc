019178b077831705c1d9624e69806498b6e311b4
Ricardo Carvalho <ricardo-svc@live.com>
2024-10-11 18:15:14 -0300

    Fix s3Config

--- src/main/java/com/criptx/cursomc/services/S3Service.java
+++ src/main/java/com/criptx/cursomc/services/S3Service.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+@Service
 public class S3Service {
 
     private Logger LOG = LoggerFactory.getLogger(S3Service.class);
