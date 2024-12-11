7e959dcadad088c49107ef1115c4936421d656dc
CriptX-42 <ricardo-svc@live.com>
2024-12-11 19:36:54 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/criptx/cursomc/services/CidadeService.java
@@ -0,0 +1,19 @@
+package com.criptx.cursomc.services;
+
+import com.criptx.cursomc.domain.Cidade;
+import com.criptx.cursomc.repositories.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CidadeService {
+
+    @Autowired
+    private CidadeRepository repo;
+
+    public List<Cidade> findByEstado(Integer estadoId) {
+        return repo.findCidades(estadoId);
+    }
+}
